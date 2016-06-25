cb9f4f0d295b4c1484e4f8a1bc9bdd6e1853f095
Ari Munandar <arimunandar.dev@gmail.com>
2016-06-25 12:34:20 +0700

    First commit

---
+++ Youtube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by Innovecto iOS on 6/25/16.
+//  Copyright © 2016 DevcastStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
